9f9e960ed727014d5b5d59ef55534a29108a2f41
Jamie MacKillop <jamieronin@gmail.com>
2017-05-12 19:22:31 -0400

    Add Advisor specific search for distances. Add email, name, company, of advisor and their distance away from input zipcode

--- app/controllers/search_advisors_controller.rb
+++ app/controllers/search_advisors_controller.rb
@@ -1,5 +1,10 @@
 class SearchAdvisorsController < ApplicationController
   def index
+    @advisors_list = []
+    Advisor.all.each do |advisor|
+      @advisors_list << advisor
+    end
+    
     @advisors = Advisor.all
     @origin = params[:client_zip_code]
     @destinations = ""
@@ -14,7 +19,11 @@ class SearchAdvisorsController < ApplicationController
 
     @response = Unirest.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{@origin}&destinations=#{@destinations}&key=AIzaSyDRvj_4M_xPyfnpWYVqkieWnBcThgRa8_M").body
 
+    @distance = []
+    @response['rows'][0]['elements'].each do |trip|
+      @distance << trip['distance']['value']
+    end
+
     render 'index.html.erb'
   end
-
 end
