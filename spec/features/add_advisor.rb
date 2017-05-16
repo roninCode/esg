35cc788b733ca55823f25e5ff15472e16b5d2184
Jamie MacKillop <jamieronin@gmail.com>
2017-05-15 23:07:22 -0400

    Add add_advisor.rb to spec/features. Testing for advisor sign up

--- spec/features/add_advisor.rb
+++ spec/features/add_advisor.rb
@@ -1,14 +1,23 @@
 require "rails_helper.rb"
-Rspec.feature ' add an advisor' do 
+
+RSpec.feature ' add an advisor' do 
   scenario " allows a user to sign up as an advisor" do
     visit "/advisors/new"
 
-    fill_in "name", with: "Random Person"
-
+    fill_in "name", with: "Devin Dutch"
+    fill_in "company", with: "Dimes and Dozens"
+    fill_in "phone", with: "123456789"
+    fill_in "zip_code", with: "11206"
+    fill_in "email", with: "d@d", :match => :first
+    # fill_in "email", with: "d@d"
+    # attach_file("logo", '/public/salad.jpg')
+    # page.all(:fillable_field, 'password').first.set('jamie')
+    # page.all(:fillable_field, 'password')[1].set('jamie')
+    page.all("password")
+    fill_in "password", with: "jamie"
+    fill_in "password_confirmation", with: "jamie"
     click_on("submit") 
 
-    expect(page).to have_content("Random Person")
+    expect(page).to have_content("Here are your Clients")
   end
-
-  
 end
